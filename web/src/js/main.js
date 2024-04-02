f958b42118ff3419085ddb2c8077a1f041cc8c00
Esta Nagy <nagyesta@gmail.com>
2024-04-02 22:42:54 +0200

    UI design - Improves alignment of movie cards - Fixes collapsible navbar
    Signed-off-by: Esta Nagy <nagyesta@gmail.com>

--- web/src/js/main.js
+++ web/src/js/main.js
@@ -1,7 +1,10 @@
 // Import our custom CSS
+// noinspection ES6UnusedImports
+
 import '../scss/styles.scss'
 
 // Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
 import * as feather from 'feather-icons'
 
 feather.replace();
