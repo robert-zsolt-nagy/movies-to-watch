003af3dd4a988dd967437942fad40ba56c89c52e
Robert Zsolt Nagy <robert.zsolt.nagy@gmail.com>
2024-04-07 18:58:29 +0200

    m2w_database handlers ready

--- src/dao/m2w_database.py
+++ src/dao/m2w_database.py
@@ -1,6 +1,6 @@
 from google.cloud import firestore
 from google.oauth2 import service_account
-from typing import Generator, Optional
+from typing import Generator, Optional, Union
 
 class M2WDatabaseException(Exception):
     """Base class for Exceptions of M2WDatabase"""
@@ -20,43 +20,94 @@ class M2WDatabase():
         credentials: The OAuth2 Credentials to use for this client.
         """
         self.__db = firestore.Client(project=project, credentials=credentials)
+        self.user = M2wUserHandler(db=self.database)
+        self.movie = M2wMovieHandler(db=self.database)
+        self.group = M2wGroupHandler(db=self.database)
 
     @property
     def database(self) -> firestore.Client:
         """The firestore database."""
         return self.__db
 
-    # user related methods
-    def get_user(self, user_id: str) -> firestore.DocumentSnapshot:
-        """Returns the user document with ID `user_id` if exists.
+class M2wDocumentHandler():
+    """An item in the movies-to-watch database."""
+    def __init__(
+            self, 
+            db: firestore.Client,
+            collection: str,
+            kind: str="Document"
+            ) -> None:
+        """Returns a represantation of a document.
+
+        Parameters
+        ----------
+        db: the client for the firestore API.
+        collection: the ID of the collection that contains the document.
+        """
+        self.__db = db
+        self.__collection = collection
+        self.__kind = kind
+
+    def get_one(self, id_: str) -> firestore.DocumentSnapshot:
+        """Returns a document with ID `id_` if exists.
         
         Raises
         ------
-        M2WDatabaseException: if user doesn't exist.
+        M2WDatabaseException: if document doesn't exist.
         """
-        user_ref = self.__db.collection('users').document(user_id)
-        user = user_ref.get()
-        if user.exists:
-            return user
+        doc_ref = self.__db.collection(self.__collection).document(id_)
+        doc = doc_ref.get()
+        if doc.exists:
+            return doc
         else:
-            raise M2WDatabaseException("User does not exist.")
+            raise M2WDatabaseException(f"{self.__kind} does not exist.")
         
-    def get_user_ref(self, user_id: str) -> firestore.DocumentReference:
-        """Returns the reference to a user with ID `user_id` 
-        if the user exists.
+    def get_all(self) -> Generator[firestore.DocumentSnapshot]:
+        """Returns a stream with all documents in the collection.
         
         Raises
         ------
-        M2WDatabaseException: if user doesn't exist.
+        M2WDatabaseException: if document doesn't exist.
         """
-        user_ref = self.__db.collection('users').document(user_id)
-        user = user_ref.get()
-        if user.exists:
-            return user_ref
-        else:
-            raise M2WDatabaseException("User does not exist.")
+        return self.__db.collection(self.__collection).stream()
+    
+    def set_data(self, id_: str, data: dict, merge: bool=True):
+        """Creates or updates a document.
         
-    def get_user_blocklist(self, user_id: str) -> firestore.CollectionReference:
+        Parameters
+        ----------
+        id_: the ID of the document in the database.
+        data: a dictionary containing the data fields of the document.
+        merge: if `True` updates the provided fields of the existing document, 
+        if `False` overwrites the existing document with the only the new content. 
+        Creates the document if it doesn't exist in both cases.
+        """
+        return self.__db.collection(self.__collection).document(id_).set(document_data=data, merge=merge)
+    
+    def delete(self, id_: str) -> bool:
+        """Deletes a document. 
+        
+        Parameters
+        ----------
+        id_: the ID of the document in the database.
+
+        Returns
+        -------
+        True is successfully deleted, False otherwise.
+        """
+        try:
+            self.__db.collection(self.__collection).document(id_).delete()
+        except:
+            return False
+        else:
+            return True
+
+
+class M2wUserHandler(M2wDocumentHandler):
+    def __init__(self, db: firestore.Client) -> None:
+        super().__init__(db, collection='users', kind='User')
+
+    def get_blocklist(self, user_id: str) -> firestore.CollectionReference:
         """Returns the reference to the blocklist collection of a user 
         with ID `user_id` if the user exists.
         
@@ -64,74 +115,43 @@ class M2WDatabase():
         ------
         M2WDatabaseException: if user doesn't exist.
         """
-        user_ref = self.__db.collection('users').document(user_id)
+        user_ref = self.__db.collection(self.__collection).document(user_id)
         user = user_ref.get()
         if user.exists:
             return user_ref.collection('blocklist')
         else:
-            raise M2WDatabaseException("User does not exist.")
+            raise M2WDatabaseException(f"{self.__kind} does not exist.")
         
-    def set_user_data(self, user_id: str, data: dict, merge: bool=True):
-        """Creates or updates a user.
-        
-        Parameters
-        ----------
-        user_id: the ID of the user in the database.
-        data: a dictionary containing the data fields of the user.
-        merge: if `True` updates the provided fields of the existing user, 
-        if `False` overwrites the existing user with the only the new content. 
-        Creates the user if it doesn't exist in both cases.
-        """
-        return self.__db.collection("users").document(user_id).set(document_data=data, merge=merge)
-        
-    def get_all_users(self) -> Generator[firestore.DocumentSnapshot]:
-        """Return a stream containing all users."""
-        return self.__db.collection("users").stream()
-    
-    # movie related methods
-    def get_movie(self, movie_id: str) -> firestore.DocumentSnapshot:
-        """Returns the movie document with ID `movie_id` if exists.
-        
-        Raises
-        ------
-        M2WDatabaseException: if the movie doesn't exist.
-        """
-        movie_ref = self.__db.collection('movies').document(movie_id)
-        movie = movie_ref.get()
-        if movie.exists:
-            return movie
-        else:
-            raise M2WDatabaseException("User does not exist.")
-    
-    def remove_movie_from_blocklist(self, user_id: str, movie_id: str) -> bool:
-        """Removes the movie from the blocklist of the user.
+class M2wMovieHandler(M2wDocumentHandler):
+    def __init__(self, db: firestore.Client) -> None:
+        super().__init__(db, collection='movies', kind="Movie")
+
+    def remove_from_blocklist(self, movie_id: str, blocklist: firestore.CollectionReference) -> bool:
+        """Removes the movie from the blocklist.
         
         Parameters
         ----------
-        user_id: ID of the memeber in m2w users
-        movie_id: ID of the movie in TMDB
+        movie_id: ID of the movie in TMDB.
+        blocklist: the reference of the affected blocklist.
 
         Returns
         -------
         True if successfull, False otherwise.
         """
         try:
-            block_ref = self.get_user_blocklist(user_id=user_id)
-            mov_ref = block_ref.document(movie_id).get()
-            if mov_ref.exists:
-                block_ref.document(movie_id).delete()
+            blocklist.document(movie_id).delete()
         except:
             return False
         else:
             return True
-    
-    def add_to_blocklist(self, user_id: str, movie_id: str, movie_title: Optional[str] = None) -> bool:
+        
+    def add_to_blocklist(self, movie_id: str, blocklist: firestore.CollectionReference, movie_title: Optional[str] = None) -> bool:
         """Adds a movie to the blocklist of the member.
         
         Parameters
         ----------
-        user_id: ID of the memeber in m2w users
         movie_id: ID of the movie in TMDB
+        blocklist: the reference of the affected blocklist.
         movie_title: the title of the movie in TMDB
 
         Returns
@@ -140,43 +160,104 @@ class M2WDatabase():
         """
         if movie_title is None:
             try:
-                movie_data = self.get_movie(movie_id=movie_id).to_dict()
+                movie_data = self.get_one(id_=movie_id).to_dict()
                 movie_title = movie_data['title']
             except:
-                pass
+                movie_title = 'unknown'
         data = {"title":movie_title}
         try:
-            self.get_user_blocklist(user_id=user_id).document(movie_id).set(data)
+            blocklist.document(movie_id).set(data)
         except:
             return False
         else:
             return True
 
+class M2wGroupHandler(M2wDocumentHandler):
+    def __init__(self, db: firestore.Client) -> None:
+        super().__init__(db, collection='groups', kind='Group')
 
-class M2wDocument(firestore.DocumentSnapshot):
-    """An item in the movies-to-watch database."""
-    def __init__(
-            self, 
-            db: firestore.Client,
-            collection: str,
-            document: str
-            ) -> None:
-        """Returns a represantation of a document.
-
-        Parameters
-        ----------
-        db: the client for the firestore API
-        collection: the ID of the collection that contains the document
-        document: the ID of the document. 
+    def get_all_group_members(self, group_id: str) -> Generator[firestore.DocumentSnapshot]:
+        """Returns a stream with all member documents in the group.
         
+        Parametes
+        ---------
+        group_id: the ID of the group in M2W Database.
+
         Raises
         ------
-        M2WDatabaseException: if the document doesn't exist.
+        M2WDatabaseException: if member document doesn't exist.
         """
-        self.__db = db
-        doc_ref = db.collection(collection).document(document)
-        doc = doc_ref.get()
-        if doc.exists:
-            return doc
+        try:
+            group_ref = self.get_one(id_=group_id).reference
+        except:
+            raise M2WDatabaseException(f"{self.__kind} does not exist.")
         else:
-            raise M2WDatabaseException("Document does not exist.")
+            return group_ref.collection('members').stream()
+        
+    def add_member_to_group(self, group_id: str, user: firestore.DocumentSnapshot) -> bool:
+        """Adds a user to the members of the group.
+        
+        Parameters
+        ----------
+        group_id: the ID of the group.
+        user: a Snapshot of the user document.
+
+        Returns
+        -------
+        True if successfull, False otherwise.
+        """
+        try:
+            group_ref = self.get_one(id_=group_id).reference
+            group_ref.collection('members').document(user.id).set(user.to_dict())
+        except:
+            return False
+        else:
+            return True
+        
+    def remove_member_from_group(self, group_id: str, user_id: str) -> bool:
+        """Adds a user to the members of the group.
+        
+        Parameters
+        ----------
+        group_id: the ID of the group.
+        user_id: the M2W ID of the user.
+
+        Returns
+        -------
+        True if successfull, False otherwise.
+        """
+        try:
+            group_ref = self.get_one(id_=group_id).reference
+            group_ref.collection('members').document(user_id).delete()
+        except:
+            return False
+        else:
+            return True
+    
+    def create_new(self, data: dict, members: Optional[Union[list[firestore.DocumentSnapshot], firestore.DocumentSnapshot]]=None) -> str:
+        """Creates a new group.
+        
+        Parameters
+        ----------
+        data: the data fields of the group.
+        memebers: a single member or a list of members for the group.
+        
+        Returns
+        -------
+        The ID for the created group
+
+        Raises
+        ------
+        M2WDatabaseException: if the group couldn't be created.
+        """
+        try:
+            _, group_ref = self.__db.collection("groups").add(document_data=data)
+            if members is not None:
+                if not isinstance(members, list):
+                    members = [members]
+                for member in members:
+                    self.add_member_to_group(group_id=group_ref.id, user=member)
+        except:
+            raise M2WDatabaseException("Could not create group.")
+        else:
+            return group_ref
